ec65842abb98b737bb8ccc505a35f83884d52ecc
MJ Habib <mj.habib4@gmail.com>
2026-06-09 09:05:13 +0330

    finish day 4 Magic 8 Ball challenge

---
+++ day_04_magic_8_ball_challenge/lib/main.dart
@@ -0,0 +1,52 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BallPage());
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
+          title: Text("Magic 8 Ball", style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: MagicBall(),
+      ),
+    );
+  }
+}
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({super.key});
+
+  @override
+  State<MagicBall> createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballNum = 1;
+
+  void randomBall() {
+    setState(() {
+      ballNum = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: randomBall,
+        child: Image.asset("assets/ball$ballNum.png"),
+      ),
+    );
+  }
+}
