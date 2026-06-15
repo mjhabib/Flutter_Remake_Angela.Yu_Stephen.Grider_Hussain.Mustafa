cbc4f915021efc24dab701565a95a3a151e613c1
MJ Habib <mj.habib4@gmail.com>
2026-06-15 08:43:29 +0330

    Init Day 9 Clima app

---
+++ day_09_clima_app_with_live_data/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '/screens/loading_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: ThemeData.dark(), home: LoadingScreen());
+  }
+}
