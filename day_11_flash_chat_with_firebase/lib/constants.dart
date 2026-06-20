3de0ac454def42a19e4312f2f6ef02c4ca5774cc
MJ Habib <mj.habib4@gmail.com>
2026-06-20 10:15:18 +0330

    Refactor code

--- day_11_flash_chat_with_firebase/lib/constants.dart
+++ day_11_flash_chat_with_firebase/lib/constants.dart
@@ -15,3 +15,19 @@ const messageTextFieldDecoration = InputDecoration(
 const messageContainerDecoration = BoxDecoration(
   border: Border(top: BorderSide(color: Colors.lightBlueAccent, width: 2.0)),
 );
+
+const textFieldDecoration = InputDecoration(
+  hintText: '',
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
