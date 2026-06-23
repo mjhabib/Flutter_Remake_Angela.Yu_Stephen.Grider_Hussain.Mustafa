ce14aaf75150015b9a9d13ebc0e8658defffe866
MJ Habib <mj.habib4@gmail.com>
2026-06-23 09:48:13 +0330

    Add primary design to the tasks_screen

--- day_12_todoey_app_with_state_management/lib/screens/tasks_screen.dart
+++ day_12_todoey_app_with_state_management/lib/screens/tasks_screen.dart
@@ -1,10 +1,78 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class TasksScreen extends StatelessWidget {
   const TasksScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.lightBlueAccent,
+        shape: CircleBorder(),
+        child: Icon(Icons.add, color: Colors.white),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 60, bottom: 30, left: 30, right: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.list,
+                    size: 30,
+                    color: Colors.lightBlueAccent,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  '12 tasks',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+          // Expanded: take as much space that is left
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text('This is a task'),
+                    trailing: Checkbox(value: false, onChanged: (value) {}),
+                  ),
+                  ListTile(
+                    title: Text('This is another task'),
+                    trailing: Checkbox(value: true, onChanged: (value) {}),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
