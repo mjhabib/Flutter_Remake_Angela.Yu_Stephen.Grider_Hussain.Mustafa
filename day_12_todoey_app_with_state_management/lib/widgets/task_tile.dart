da8773870717109d312aac5bf93f3e58bb94325d
MJ Habib <mj.habib4@gmail.com>
2026-06-23 17:32:26 +0330

    Add a ListView.builder to create a list of tasks dynamically

--- day_12_todoey_app_with_state_management/lib/widgets/task_tile.dart
+++ day_12_todoey_app_with_state_management/lib/widgets/task_tile.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
-  const TaskTile({super.key});
+  const TaskTile({
+    super.key,
+    required this.name,
+    required this.value,
+    required this.onChanged,
+  });
+
+  final String name;
+  final bool value;
+  final ValueChanged<bool?>? onChanged;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: Checkbox(value: false, onChanged: (value) {}),
+      title: Text(
+        name,
+        style: TextStyle(decoration: value ? TextDecoration.lineThrough : null),
+      ),
+      trailing: Checkbox(
+        checkColor: Colors.white,
+        activeColor: Colors.lightBlueAccent,
+        value: value,
+        onChanged: onChanged,
+      ),
     );
   }
 }
--- day_12_todoey_app_with_state_management/lib/widgets/tasks_list.dart
+++ day_12_todoey_app_with_state_management/lib/widgets/tasks_list.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
+import 'package:todoey_app/models/task.dart';
 import 'package:todoey_app/widgets/task_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
   const TasksList({super.key});
 
   @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  bool isChecked = false;
+  List<Task> tasks = [Task(name: 'Buy milk'), Task(name: 'Buy Bread')];
+
+  @override
   Widget build(BuildContext context) {
-    return ListView(children: [TaskTile(), TaskTile()]);
+    // instead of using a regular ListView to build a list of tasks, the builder only builds as many items as we can see (or fit the screen). That way, if we have hundreds of tasks, we're gonna save a lot of memory/recourses.
+    // the itemCount builds as many as tasks we have in our list
+    return ListView.builder(
+      itemCount: tasks.length,
+      itemBuilder: (context, index) {
+        return TaskTile(
+          name: tasks[index].name,
+          value: tasks[index].isDone,
+          onChanged: (newValue) {
+            setState(() {
+              tasks[index].toggleDone();
+            });
+          },
+        );
+      },
+    );
   }
 }
