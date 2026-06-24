b7d6d41e952ff2b4018b2acc2fd7feaacddc0665
MJ Habib <mj.habib4@gmail.com>
2026-06-24 10:10:31 +0330

    Init Day 13 Shopping app with Riverpod

---
+++ day_13_shopping_app_with_riverpod/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'package:shopping_app/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
