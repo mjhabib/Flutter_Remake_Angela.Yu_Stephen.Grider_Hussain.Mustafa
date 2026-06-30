4d0b1a3fd2277ad08cf02fd3e78c495b03e0696e
MJ Habib <mj.habib4@gmail.com>
2026-06-30 12:14:11 +0330

    Add a user-case of a listen()/listenManual() method inside a ConsumerStatefulWidget with the access to its ConsumerState

--- day_14_riverpod_practice/lib/widgets/counter_button.dart
+++ day_14_riverpod_practice/lib/widgets/counter_button.dart
@@ -3,11 +3,36 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:riverpod_practice/providers/counter_provider.dart';
 
-class CounterButton extends ConsumerWidget {
+// Since a 'ConsumerWidget' is stateless and its build() method can be called many times (we could accidentally show the SnackBar multiple times), so it's not reasonable to put our listener inside this class, that's why we need to convert it to a 'ConsumerStatefulWidget', which means now we have access to methods like initState() and dispose() which has a lifecycle (and runs only once).
+class CounterButton extends ConsumerStatefulWidget {
   const CounterButton({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<CounterButton> createState() => _CounterButtonState();
+}
+
+// 'ConsumerState' will gives us access to 'ref' everywhere in the state class.
+class _CounterButtonState extends ConsumerState<CounterButton> {
+  // Now that we have access to the initState() and we know it only runs once, now we can put our listener inside it.
+  @override
+  void initState() {
+    super.initState();
+    // In Riverpod 3.x or above we should replace listen() with listenManual() inside lifecycle widgets like this, because listenManual() creates a listener that lives outside the widget build process and Riverpod manages its lifecycle for us.
+    ref.listenManual(counterProvider, (previous, next) {
+      if (next == 10 || next == 20) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text(
+              'Congratulation, you pressed the add button 10 times!',
+            ),
+          ),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     // A: counterProvider → "What's the current value?"
     final counter = ref.watch(counterProvider);
     return Column(
