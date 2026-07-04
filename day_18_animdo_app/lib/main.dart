5c84ed781da0bc4a671f3976a94a8567a708238e
MJ Habib <mj.habib4@gmail.com>
2026-07-04 08:56:59 +0330

    Add a simple UI design to begin the animating part

--- day_18_animdo_app/lib/main.dart
+++ day_18_animdo_app/lib/main.dart
@@ -4,14 +4,44 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Animdo App',
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      home: Scaffold(
+        body: Stack(
+          clipBehavior: Clip.none,
+          children: [screenBackground(), circularButton()],
+        ),
+      ),
+    );
+  }
+
+  Widget screenBackground() {
+    return Container(color: Colors.blueAccent);
+  }
+
+  Widget circularButton() {
+    return Center(
+      child: GestureDetector(
+        onTap: () {},
+        child: Container(
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+            color: Colors.purpleAccent,
+            borderRadius: BorderRadius.circular(100),
+          ),
+        ),
+      ),
     );
   }
 }
