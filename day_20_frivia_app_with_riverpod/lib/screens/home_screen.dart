17949d721fd56e779b877a9ea98a9e97f17d3c4c
MJ Habib <mj.habib4@gmail.com>
2026-07-05 12:58:47 +0330

    Add a few widgets to the UI

--- day_20_frivia_app_with_riverpod/lib/screens/home_screen.dart
+++ day_20_frivia_app_with_riverpod/lib/screens/home_screen.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+  double? deviceHeight, deviceWidth;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Hello World!')));
+    deviceHeight = MediaQuery.of(context).size.height;
+    deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: deviceHeight! * 0.05),
+          child: gameUI(),
+        ),
+      ),
+    );
+  }
+
+  Widget gameUI() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        questionText(),
+        Center(
+          child: Column(
+            children: [
+              trueFalseButton('True', Colors.greenAccent, () {}),
+              SizedBox(height: deviceHeight! * 0.01),
+              trueFalseButton('False', Colors.redAccent, () {}),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget questionText() {
+    return Text(
+      'Some questions',
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w400,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  Widget trueFalseButton(
+    String label,
+    Color selectedColor,
+    VoidCallback? onPressed,
+  ) {
+    return MaterialButton(
+      onPressed: onPressed,
+      color: selectedColor,
+      minWidth: deviceWidth! * 0.80,
+      height: deviceHeight! * 0.10,
+      child: Text(label, style: TextStyle(color: Colors.white, fontSize: 20)),
+    );
   }
 }
