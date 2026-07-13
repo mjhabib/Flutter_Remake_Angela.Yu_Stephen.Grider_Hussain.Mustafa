05fe85ac7213eed432e48308c85832cea2363944
MJ Habib <mj.habib4@gmail.com>
2026-07-13 11:01:44 +0330

    Create a list of all 52 cards using for loops

--- day_23_deck_of_cards_with_dart/bin/deck_of_cards.dart
+++
@@ -1,5 +0,0 @@
-import 'package:deck_of_cards/deck_of_cards.dart' as deck_of_cards;
-
-void main(List<String> arguments) {
-  print('Hello world: ${deck_of_cards.calculate()}!');
-}
--- day_23_deck_of_cards_with_dart/lib/deck_of_cards.dart
+++ day_23_deck_of_cards_with_dart/lib/deck_of_cards.dart
@@ -1,3 +1,38 @@
-int calculate() {
-  return 6 * 7;
+class Card {
+  String rank;
+  String suit;
+
+  Card(this.rank, this.suit);
+}
+
+class Deck {
+  List<Card> cards = [];
+
+  // initialize a list of 52 cards the moment we call the Deck class
+  Deck() {
+    var ranks = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+    var suits = [
+      'Ace',
+      'One',
+      'Two',
+      'Three',
+      'Four',
+      'Five',
+      'Six',
+      'Seven',
+      'Eight',
+      'Nine',
+      'Ten',
+      'Jack',
+      'Queen',
+      'King',
+    ];
+
+    for (var rank in ranks) {
+      for (var suit in suits) {
+        var card = Card(rank, suit);
+        cards.add(card);
+      }
+    }
+  }
 }
