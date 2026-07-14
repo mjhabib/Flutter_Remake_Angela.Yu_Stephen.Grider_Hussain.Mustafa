fa0f33f438d16adba83741b51b1adbe75add8fd5
MJ Habib <mj.habib4@gmail.com>
2026-07-14 16:48:14 +0330

    Init Day 25 Login_Stateful app

---
+++ day_25_login_stateful/lib/src/main_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Log Me In!',
+      home: Scaffold(body: Center(child: Text('Show a form here!!'))),
+    );
+  }
+}
