848007a2d42d3f5e8f7965b4e212e1404249c6f9
MJ Habib <mj.habib4@gmail.com>
2026-07-16 09:28:25 +0330

    Add a few widgets to work with to the UI

--- day_26_login_riverpod/lib/screens/login_screen.dart
+++ day_26_login_riverpod/lib/screens/login_screen.dart
@@ -5,6 +5,39 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: EdgeInsets.all(20),
+      // we didn't use a Form here because in the old app it acted as a stateful-widget, but now that we're using a provider like riverpod, it can be ignored
+      child: Column(
+        children: [
+          emailField(),
+          SizedBox(height: 10),
+          passwordField(),
+          SizedBox(height: 20),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@email.com',
+        labelText: 'Email:',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(hintText: 'abc123', labelText: 'Password:'),
+    );
+  }
+
+  Widget submitButton() {
+    return ElevatedButton(onPressed: () {}, child: Text('Submit'));
   }
 }
