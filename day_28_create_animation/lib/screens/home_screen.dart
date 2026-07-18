906d93f86b9dcc956e6eb4ebf787eff771ee9137
MJ Habib <mj.habib4@gmail.com>
2026-07-18 21:02:05 +0330

    Animate the cat image (TickerProviderStateMixin, Animation, AnimationController, Tween, CurvedAnimation, AnimatedBuilder)

--- day_28_create_animation/lib/screens/home_screen.dart
+++ day_28_create_animation/lib/screens/home_screen.dart
@@ -8,13 +8,43 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  late Animation<double> catAnimation;
+  late AnimationController catController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    catController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 30),
+    );
+
+    catAnimation = Tween(
+      begin: 0.0,
+      end: 100.0,
+    ).animate(CurvedAnimation(parent: catController, curve: Curves.easeIn));
+
+    catController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return buildAnimation();
   }
 
   Widget buildAnimation() {
-    return CatImage();
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        // the container is an inexpensive widget to recreate/build 60 times/s (animate) but the CatImage can be expensive to build, that's why we move/animate the container not the cat
+        return Container(
+          margin: EdgeInsets.only(top: catAnimation.value),
+          child: child,
+        );
+      },
+      child: CatImage(),
+    );
   }
 }
