86aefcb3c061af8f133c17d24457037f2194209b
MJ Habib <mj.habib4@gmail.com>
2026-07-20 18:05:03 +0330

    Add a class called repository to manage both the DB and the API calls + cast a return type from dynamic to int

--- day_29_news_app/lib/resources/news_api_provider.dart
+++ day_29_news_app/lib/resources/news_api_provider.dart
@@ -8,7 +8,8 @@ class NewsApiProvider {
   final Client _client = Client();
 
   // get all the top news IDs
-  Future<List<dynamic>> fetchTopIds() async {
+  // we could type this method as List<dynamic> and move on but we know we're gonna return a list of ids which is the type int, but the dart doesn't know that. So, if we change the type to List<int> dart is gonna complain, that's why we're gonna cast this method as int first then change the return type to List<int>.
+  Future<List<int>> fetchTopIds() async {
     try {
       final response = await _client.get(
         Uri.parse('$_newsEndpoint/topstories.json'),
@@ -16,7 +17,7 @@ class NewsApiProvider {
 
       if (response.statusCode >= 200) {
         final ids = json.decode(response.body);
-        return ids;
+        return ids.cast<int>();
       } else {
         throw ('Failed to load item: ${response.statusCode}');
       }
---
+++ day_29_news_app/lib/resources/repository.dart
@@ -0,0 +1,29 @@
+import 'package:news_app/models/item_model.dart';
+import 'package:news_app/resources/news_api_provider.dart';
+import 'package:news_app/resources/news_db_provider.dart';
+
+class Repository {
+  NewsApiProvider apiProvider = NewsApiProvider();
+  NewsDbProvider dbProvider = NewsDbProvider();
+
+  Future<List<dynamic>> fetchTopIds() {
+    return apiProvider.fetchTopIds();
+  }
+
+  Future<ItemModel> fetchItem(int id) async {
+    // check to see if data exists in the DB or not
+    var item = await dbProvider.fetchItem(id);
+    if (item != null) {
+      return item;
+    }
+
+    // if we didn't find any data in the DB, we fetch it using an API call
+    item = await apiProvider.fetchItem(id);
+
+    // then we save it to the DB for the future calls
+    dbProvider.addItem(item);
+
+    // now that we know we have data, we can return it
+    return item;
+  }
+}
